fb808298c77158f0b891e0be79d3fa187720a0be
Artemon4ik8091 <yandex8091@gmail.com>
2026-03-04 23:13:42 +0300

    Initial Commit

---
+++ media player/media_playerApp.swift
@@ -0,0 +1,17 @@
+//
+//  media_playerApp.swift
+//  media player
+//
+//  Created by aswer on 04.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct media_playerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
