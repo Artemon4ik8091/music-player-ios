df8475e6d165c9bab49c52dd92f5b6db47466421
aswer <aswer@MacBook-Pro-aswer.local>
2026-06-24 10:50:09 +0300

    Initial commit

---
+++ music player/music_playerApp.swift
@@ -0,0 +1,17 @@
+//
+//  music_playerApp.swift
+//  music player
+//
+//  Created by aswer on 23.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct music_playerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
